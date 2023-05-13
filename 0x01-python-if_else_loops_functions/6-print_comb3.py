79e1bb117172c1cbd664d418c0026b6086e12585
ndeyeamy <falamy2006@gmail.com>
2023-05-12 18:02:53 -0700

    7---102

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -11,4 +11,7 @@ for digit1 in range(0, 10):
             print("{}{}".format(digit1, digit2))
         else:
             print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
 

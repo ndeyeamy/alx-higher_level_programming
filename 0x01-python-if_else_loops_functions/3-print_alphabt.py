99f4119316be688d0079e4da18975bdfe612e8ca
ndeyeamy <falamy2006@gmail.com>
2023-08-15 12:39:17 -0700

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(4):
-    print(chr(97 + i), end = "")
+    print("{}".format(chr(97 + i)), end="")
 for i in range(10):
-    print(chr(102 + i), end = "")
+    print("{}".format(chr(102 + i)), end="")
 for i in range(8):
-    print(chr(113 + i), end = "")
+    print("{}".format(chr(113 + i)), end="")

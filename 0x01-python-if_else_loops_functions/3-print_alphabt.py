cedfae94fd67595f14caca076b60ea023d56c70a
ndeyeamy <falamy2006@gmail.com>
2023-08-15 12:43:45 -0700

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(4):
-    print("{}".format(chr(97 + i)), end="")
-for i in range(10):
-    print("{}".format(chr(102 + i)), end="")
-for i in range(8):
-    print("{}".format(chr(113 + i)), end="")
+for i in range(26):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")

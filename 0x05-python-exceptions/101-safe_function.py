b7a1fe38107aca5e78f21a7093f3ae673f3f997e
ndeyeamy <falamy2006@gmail.com>
2023-08-28 09:36:02 -0700

    all

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except result.DoesNotExist:
+    except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
